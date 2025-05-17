24a762cbd0a264e9460e18d26a9aeb13666c4eb2
Kit-tw <kittikhun.tw@gmail.com>
2025-05-18 03:21:45 +0700

    edit a little

--- api/start-app.sh
+++ api/start-app.sh
@@ -2,7 +2,7 @@
 . /usr/src/app/venv/bin/activate
 
 # Run checkpython.js with node
-node /usr/src/app/checkpython.js &
+# node /usr/src/app/checkpython.js &
 
 # Start the Python API in the background
 #python /usr/src/app/python/API.py &
